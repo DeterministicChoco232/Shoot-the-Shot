a158ed9e5138b89745fd058b14e79a32e1c195dd
paulclavaud <paul.a.clavaud@gmail.com>
2026-02-21 11:37:45 -0500

    updated arduino script

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,6 +3,19 @@
 Controller controller("NoahsArch", "SurvivedTheFlood");
 void setup() {
   Serial.begin(115200);
+
+  // Configure pins
+  controller.configureL298N(9,7,6,10,5,4);
+
+  // Set motor PWM for a medium-weight robot
+  controller.setMotorMinPWM(90);
+
+  // Stops after no command received
+  controller.setFailsafeTimeoutMs(1200);
+
+  // Enable status LED
+  controller.enableStatusLED(LED_BUILTIN);
+
   controller.beginAP(true);   // true = enable debug
 }
 
